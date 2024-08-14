a642b0126cbe147560b8b719c592b6690f663d25
practicantess <ccandido@sistemasenergeticos.com.ar>
2024-08-14 09:04:03 -0300

    head y header
    head y header cambiados a archivos indv para incluirlos

---
+++ assets/includes/head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- Las 3 etiquetas meta anteriores *deben* ir primero en el head; cualquier otro contenido en el head debe ir *después* de estas etiquetas -->
+
+		<title>Electro - Plantilla HTML de Comercio Electrónico</title>
+
+ 		<!-- Fuente de Google -->
+ 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+ 		<!-- Bootstrap -->
+ 		<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css"/>
+
+ 		<!-- Slick -->
+ 		<link type="text/css" rel="stylesheet" href="../css/slick.css"/>
+ 		<link type="text/css" rel="stylesheet" href="../css/slick-theme.css"/>
+
+ 		<!-- nouislider -->
+ 		<link type="text/css" rel="stylesheet" href="../css/nouislider.min.css"/>
+
+ 		<!-- Icono de Font Awesome -->
+ 		<link rel="stylesheet" href="../css/font-awesome.min.css">
+
+ 		<!-- Hoja de estilo personalizada -->
+ 		<link type="text/css" rel="stylesheet" href="../css/style.css"/>
---
+++ assets/includes/header.php
@@ -0,0 +1,123 @@
+<header>
+			<!-- CABECERA SUPERIOR -->
+			<div id="top-header">
+				<div class="container">
+					<ul class="header-links pull-left">
+						<li><a href="#"><i class="fa fa-phone"></i> +021-95-51-84</a></li>
+						<li><a href="#"><i class="fa fa-envelope-o"></i> email@email.com</a></li>
+						<li><a href="#"><i class="fa fa-map-marker"></i> 1734 Stonecoal Road</a></li>
+					</ul>
+					<ul class="header-links pull-right">
+						<li><a href="#"><i class="fa fa-dollar"></i> USD</a></li>
+						<li><a href="#"><i class="fa fa-user-o"></i> Mi Cuenta</a></li>
+					</ul>
+				</div>
+			</div>
+			<!-- /CABECERA SUPERIOR -->
+
+			<!-- CABECERA PRINCIPAL -->
+			<div id="header">
+				<!-- contenedor -->
+				<div class="container">
+					<!-- fila -->
+					<div class="row">
+						<!-- LOGO -->
+						<div class="col-md-3">
+							<div class="header-logo">
+								<a href="#" class="logo">
+									<img src="./img/logo.png" alt="">
+								</a>
+							</div>
+						</div>
+						<!-- /LOGO -->
+
+						<!-- BARRA DE BÚSQUEDA -->
+						<div class="col-md-6">
+							<div class="header-search">
+								<form>
+									<select class="input-select">
+										<option value="0">Todas las Categorías</option>
+										<option value="1">Categoría 01</option>
+										<option value="1">Categoría 02</option>
+									</select>
+									<input class="input" placeholder="Buscar aquí">
+									<button class="search-btn">Buscar</button>
+								</form>
+							</div>
+						</div>
+						<!-- /BARRA DE BÚSQUEDA -->
+
+						<!-- CUENTA -->
+						<div class="col-md-3 clearfix">
+							<div class="header-ctn">
+								<!-- Lista de deseos -->
+								<div>
+									<a href="#">
+										<i class="fa fa-heart-o"></i>
+										<span>Tu Lista de Deseos</span>
+										<div class="qty">2</div>
+									</a>
+								</div>
+								<!-- /Lista de deseos -->
+
+								<!-- Carrito -->
+								<div class="dropdown">
+									<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+										<i class="fa fa-shopping-cart"></i>
+										<span>Tu Carrito</span>
+										<div class="qty">3</div>
+									</a>
+									<div class="cart-dropdown">
+										<div class="cart-list">
+											<div class="product-widget">
+												<div class="product-img">
+													<img src="./img/product01.png" alt="">
+												</div>
+												<div class="product-body">
+													<h3 class="product-name"><a href="#">nombre del producto</a></h3>
+													<h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
+												</div>
+												<button class="delete"><i class="fa fa-close"></i></button>
+											</div>
+
+											<div class="product-widget">
+												<div class="product-img">
+													<img src="./img/product02.png" alt="">
+												</div>
+												<div class="product-body">
+													<h3 class="product-name"><a href="#">nombre del producto</a></h3>
+													<h4 class="product-price"><span class="qty">3x</span>$980.00</h4>
+												</div>
+												<button class="delete"><i class="fa fa-close"></i></button>
+											</div>
+										</div>
+										<div class="cart-summary">
+											<small>3 artículo(s) seleccionados</small>
+											<h5>SUBTOTAL: $2940.00</h5>
+										</div>
+										<div class="cart-btns">
+											<a href="#">Ver Carrito</a>
+											<a href="#">Pagar  <i class="fa fa-arrow-circle-right"></i></a>
+										</div>
+									</div>
+								</div>
+								<!-- /Carrito -->
+
+								<!-- Menú Toggle -->
+								<div class="menu-toggle">
+									<a href="#">
+										<i class="fa fa-bars"></i>
+										<span>Menú</span>
+									</a>
+								</div>
+								<!-- /Menú Toggle -->
+							</div>
+						</div>
+						<!-- /CUENTA -->
+					</div>
+					<!-- /fila -->
+				</div>
+				<!-- /contenedor -->
+			</div>
+			<!-- /CABECERA PRINCIPAL -->
+		</header>
