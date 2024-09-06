94dee89e36fe6e66cce5b795650c10f006cbab64
practicantess <ccandido@sistemasenergeticos.com.ar>
2024-09-06 15:25:10 -0300

    Banners-sliders

--- index.php
+++ index.php
@@ -33,23 +33,23 @@ if ($stmt->execute()) {
 <?php include 'assets/includes/head.php';?>
 <style>
     .swiper-container {
-      width: 100%;
-      padding-top: 20px;
-      padding-bottom: 40px;
-	  height: 500px;
+		width: 100%;
+		padding-top: 20px;
+		padding-bottom: 40px;
+		height: 500px;
     }
     .swiper-slide {
-      text-align: center;
-	  width: 100%;
-      font-size: 18px;
-      background: #fff;
-      display: flex !important;
-      justify-content: center;
-      align-items: center;
-	  height: 500px;
-	  background-position: center;
-	  background-repeat: no-repeat;
-	  background-size: cover;
+		text-align: center;
+		width: 100%;
+		font-size: 18px;
+		background: #fff;
+		display: flex !important;
+		justify-content: center;
+		align-items: center;
+		height: 500px;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
     }
 	.swiper-slide .btn{
 		align-self: end;
@@ -65,51 +65,240 @@ if ($stmt->execute()) {
 		margin-top: 25px;
 	}
 	/* Estilos para los botones de navegación personalizados */
-.custom-button-next,
-.custom-button-prev {
-  cursor: pointer;
-  width: 40px;
-  height: 40px;
-  color: gray; /* Color del texto */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 25%; /* Hace los botones redondos */
-  position: relative; /* Posicionamiento absoluto para ubicarlos en el contenedor */
-  top: 50%; /* Centrados verticalmente */
-  font-size: 28px !important;
-  z-index: 10; /* Asegura que estén sobre otros elementos */
-  transition: background-color 0.3s; /* Transición para un efecto suave al pasar el ratón */
+	.custom-button-next,
+	.custom-button-prev {
+		cursor: pointer;
+		width: 40px;
+		height: 40px;
+		color: gray; /* Color del texto */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 25%; /* Hace los botones redondos */
+		position: relative; /* Posicionamiento absoluto para ubicarlos en el contenedor */
+		top: 50%; /* Centrados verticalmente */
+		font-size: 28px !important;
+		z-index: 10; /* Asegura que estén sobre otros elementos */
+		transition: background-color 0.3s; /* Transición para un efecto suave al pasar el ratón */
 }
-.custom-button-next span,
-.custom-button-prev  span{
-   font-size: 48px !important;
+	.custom-button-next span,
+	.custom-button-prev  span{
+		font-size: 48px !important;
+	}
+	.custom-button-next:hover,
+	.custom-button-prev:hover {
+	  	background-color: #0056b3; /* Cambio de color al pasar el ratón */
+		color: white;
+	}
+	.custom-button-next {
+		margin-left: auto;
+	}
+	.custom-button-prev {
+		margin-right: auto;
+	}
+	.control-slider {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		top: 55%;
+	}
+	#dimensionador{
+		background-color: #D51939;
+	}
+.second-s a{
+    border: 1px solid transparent;
+    padding: 10px 54px;
+    color: #D51939;
+    background-color: white;
+    text-transform: uppercase;
+    font-weight: 700;
+    border-radius: 40px;
+    -webkit-transition: 0.2s all;
+    transition: 0.2s all;
+    font-size: 25px;
 }
-.custom-button-next:hover,
-.custom-button-prev:hover {
-  background-color: #0056b3; /* Cambio de color al pasar el ratón */
-  color: white;
+.first-p p{
+    color: white;
+    font-size: 34px;
+    font-weight: 600;
+    margin-bottom: 60px;
 }
-
-.custom-button-next {
-margin-left: auto;
+.second-s img{
+    width: 150px;
 }
-
-.custom-button-prev {
-margin-right: auto;
+.second-s{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
 }
-.control-slider {
+/* Estilo para el slide 2 */
+#slide-2 {
+    position: relative;
+}
+.swiper-notification{
+	display: none;
+}
+.banner-2 {
+	position: relative;
+    width: 100%;
+    height: 100%; /* Toda la altura de la pantalla */
+    background-image: url('assets/images/prueba.png'); /* Imagen de fondo */
+    background-size: cover;
+    background-position: center;
     display: flex;
     justify-content: center;
     align-items: center;
-    position: relative;
-    top: 55%;
-}
-#dimensionador{
-	background-image: url(assets/images/dimensionador.png);
 }
 
-  </style>
+.overlay-2 {
+	position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(54, 99, 171, 0.6)); 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.content-2 {
+    text-align: center;
+    z-index: 2; /* El contenido debe estar sobre la imagen */
+	color: white !important;
+
+}
+
+.content-2 h1 {
+	font-size: 3.5rem;
+    font-weight: bold;
+    margin-bottom: 20px;
+	color: white ;
+}
+
+.content-2 p {
+    font-size: 2.2rem;
+    margin-bottom: 10px;
+}
+
+.content-2 span {
+	font-size: 2.5rem;
+    font-weight: bold;
+    text-transform: uppercase;
+    border-bottom: 2px solid white; /* Subrayado */
+}
+
+div#slider-3 {
+    background-color: #000;
+    display: flex;
+}
+.banner-3 {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    color: white;
+    background-color: #000; /* Fondo negro */
+}
+
+.content-3 {
+    flex: 1;
+	height: 320px;
+}
+
+.header-3 .ribbon {
+    background-color: #E30613;
+    padding: 10px 18px;
+    border-top-right-radius: 35px;
+    border-bottom-right-radius: 35px;
+    color: white;
+    margin-bottom: 70px;
+}
+
+.header-3 .ribbon h2 {
+    margin: 0;
+    font-size: 3.5rem;
+    font-weight: bold;
+	color: white;
+}
+
+.description-3 h1 {
+    font-size: 2rem;
+    margin-bottom: 20px;
+	color: white;
+}
+.description-3 {
+	display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.description-3 ul {
+    font-size: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.description-3 li {
+    margin-bottom: 10px;
+    font-weight: 300;
+}
+
+.product-image-3 {
+    flex: 1;
+    text-align: right;
+}
+
+.product-image-3 img {
+    max-width: 450px;
+    height: auto;
+	margin-right: 4rem;
+}
+
+@media (max-width: 700px){
+    .second-s {
+        display: flex;
+        align-items: center;
+        flex-direction: column-reverse;
+    }
+    .second-s img{
+        width: 100px;
+    }
+    .second-s a{
+        padding: 8px 40px;
+        font-size: 20px;
+        margin-bottom: 40px;
+    }
+    .first-p p {
+        font-size: 28px;
+        margin-bottom: 40px;
+    }
+    .banner-3 {
+        flex-direction: column; /* Coloca la imagen y el contenido en columna */
+        align-items: center; /* Centra el contenido */
+        text-align: center;
+    }
+
+    .product-image-3 img {
+        max-width: 250px; /* Reduce el tamaño de la imagen */
+        margin-top: 20px; /* Añade espacio superior */
+    }
+
+    .header-3 .ribbon h2 {
+        font-size: 2rem; /* Ajusta el tamaño del texto */
+    }
+
+    .description-3 h1 {
+        font-size: 1.5rem;
+    }
+
+    .description-3 ul {
+        font-size: 1.2rem;
+    }
+
+}
+    </style>
     <!-- CSS de Swiper -->
 	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 </head>
@@ -124,37 +313,59 @@ margin-right: auto;
 			<!-- container -->
 			<div class="container" style="overflow: hidden;">
             <!-- Controles de navegación -->
-					<div class="swiper-container">
-						<div class="control-slider">
-						<div class="custom-button-prev"><span class="material-symbols-outlined">
-chevron_left
-</span></div>
-						<div class="custom-button-next"><span class="material-symbols-outlined">
-chevron_right
-</span></div>
-						</div>
-						<!-- Wrapper de Slides -->
-						<div class="swiper-wrapper">
-						<!-- Slide 1 -->
-						<div class="swiper-slide" id="dimensionador">
-							<button class="btn btn-primary">Dimensiona ya!</button>
-						</div>
-						<!-- Slide 2 -->
-						<div class="swiper-slide" id="2222">
-							<button class="btn btn-success">Botón 2</button>
-						</div>
-						<!-- Slide 3 -->
-						<div class="swiper-slide" id="9999">
-							<button class="btn btn-danger">Botón 3</button>
-						</div>
-						<!-- Agregar más slides según sea necesario -->
+				<div class="swiper-container">
+					<div class="control-slider">
+						<div class="custom-button-prev"><span class="material-symbols-outlined">chevron_left</span></div>
+						<div class="custom-button-next"><span class="material-symbols-outlined">chevron_right</span></div>
 					</div>
-
-            <!-- Paginación -->
-          </div>
-		  <div class="swiper-pagination"></div>
-      </div>
-	  
+					<div class="swiper-wrapper"><!-- Slide 1 -->
+						<div class="swiper-slide" id="dimensionador">
+                            <div class="slide-1">
+                                <div class="first-p">
+                                    <p>¿Queres acceder a nuestra lista de precios?</p>
+                                </div>
+                                <div class="second-s">  
+                                    <img src="assets/images/verified.png" alt="">
+									<a href="">Verificate</a>
+                                </div>
+                            </div>
+                        </div>
+						<div class="swiper-slide" id="slide-2">	<!-- Slide 2 -->
+							<div class="banner-2">
+								<div class="overlay-2">
+									<div class="content-2">
+									<h1>¿Probaste nuestro dimensionador?</h1>
+										<p>Dimensioná tu sistema en tan solo unos pasos</p>
+										<span>TOTALMENTE GRATIS</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="swiper-slide" id="slider-3"><!-- Slide 3 -->
+							<div class="banner-3">
+								<div class="content-3">
+									<div class="header-3">
+										<div class="ribbon">
+											<h2>Nuevo lanzamiento!</h2>
+										</div>
+									</div>
+									<div class="description-3">
+										<h1>Micro-inversor GoodWe</h1>
+										<ul>
+											<li><span style="color:#d10024;">✔</span> Soporta hasta 4 paneles</li>
+											<li><span style="color:#d10024;">✔</span>  WiFi y Bluetooth integrados</li>
+										</ul>
+									</div>
+								</div>
+								<div class="product-image-3">
+									<img src="assets/images/m-inversor.png" alt="Micro-inversor GoodWe">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="swiper-pagination"></div>
+			</div>
 			<div class="container">
 				<!-- row -->
 				<div class="row">
@@ -598,12 +809,9 @@ var x = setInterval(function() {
     },
     navigation: {
 		nextEl: '.custom-button-next', // Apunta al nuevo botón personalizado
-		prevEl: '.custom-button-prev', // Apunta al nuevo botón personalizado
+		prevEl: '.custom-button-prev', // Apunta al nuevo botón personalizado AQQQQQUo
     },
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
-    },
+    
     effect: 'slide', // Aquí puedes cambiar el efecto a 'fade', 'cube', etc.
   });
 </script>
